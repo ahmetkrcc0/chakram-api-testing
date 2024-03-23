21779c03e26d6c5c8c7fd7da7b7a0e7311546dde
ahmetkrcc0 <ahmetkrcc@gmail.com>
2024-03-23 21:27:44 +0300

    addTest

---
+++ tests/userTests.js
@@ -0,0 +1,13 @@
+const chakram=require('chakram');
+
+describe("userTest", function() {
+
+     it("GET /user/{username} getUserInfo", function () {
+     
+     });
+
+    it("TC0002 /v2/user/{userName}", function () {
+     // Test kodlarınız
+     });
+
+ });
